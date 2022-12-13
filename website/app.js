9fc73318821669849c219b9bce2c92f1618c3401
Lee Santos <lee.m.santos@outlook.com>
2022-12-13 12:02:37 -0500

    Constructed home page. No JS/animations.

--- website/app.js
+++ website/app.js
@@ -5,8 +5,8 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
-app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get("/", function(req, res) {
     res.sendFile(__dirname + "/index.html");
---
+++ website/public/index.js
@@ -0,0 +1,1 @@
+$("body").css("background-color", "#0B0D17");
